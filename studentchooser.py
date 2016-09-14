8787862f3e1f9e98afd6b231ec19ca756d0e2f0b
Sachal Malick <sachalmalick@Sachals-MacBook-Air.local>
2016-09-13 21:48:05 -0400

    initial commit

---
+++ studentchooser.py
@@ -0,0 +1,28 @@
+from random import choice
+
+CLASSES =  {
+    4: [ 'Ayman', 'Shaeq', 'Patrick', 'Yvonne', 'Wilson',
+         'Brian', 'Farhan', 'Janet', 'Harry', 'Kevin',
+         'Nicholas', 'Jason', 'Yikai', 'Emma', 'Kenneth',
+         'Nala', 'Karol', 'Elias', 'Ely', 'Reo', 'Dhriaj',
+         'Amy', 'Arvind', 'Nobel', 'Angela', 'Edward',
+         'Jonathan', 'Celine', 'Daniel', 'Lindsey', 'Ziyan', 'Elina'],
+    8: ['Julian', 'Sebastian', 'Jordan', 'Alan', 'Yuki',
+        'Chloe', 'Noah', 'Edvic', 'Jia Qi', 'Shan', 'Rodda',
+        'Anya', 'Edmond', 'Asher', 'Kathy', 'Sharon', 'Vncent',
+        'Lawrence', 'Sachal', 'Gabriel', 'Jason', 'Daniel',
+        'Felix', 'Jacob', 'Bayle', 'Fortune', 'Gio',
+        'Kelly', 'William', 'Jordan', 'Haley', 'Henry'],
+    9: ['James', 'Vanna', 'Zicheng', 'Quinn', 'Anthony C',
+        'Joel', 'Steph', 'Xinhui', 'Richard', 'Misha',
+        'Maddie', 'Winston', 'Shariar', 'Nancy', 'Jerry',
+        'Michael', 'Stiven', 'Will',  'Olivia', 'Constantine',
+        'Jessica', 'Kate', 'Shamaul', 'Max', 'Sarah', 'Anthony L',
+        'Brandon', 'Nicole', 'Brian', 'Issac', 'Seiji', 'Lorenz']
+}
+
+name=raw_input('Enter a period : ')
+period = int(name)
+
+print choice(CLASSES[period])
+ 
